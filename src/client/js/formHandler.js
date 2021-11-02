4500eabbaac052a49b5cb32947740b37496205a2
rlyap93 <rlyap93@yahoo.com>
2021-11-02 21:13:32 +1100

    Initial commit

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,43 @@
+// Personal API Key for OpenWeatherMap API
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+const apiKey = '62eac2ad377f4082e0e45d28a4c12900';
+let city = '';
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
+    if(Client.checkForName(formText)){
+        city = 'London';
+    }
+    else{
+        city = 'Dublin';
+    }
+
+    getWeatherData(baseURL,city, apiKey)
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.main.temp + "&#8451; in " + res.name;
+    })
+
+    console.log("::: Form Submitted :::")
+    fetch('http://localhost:8081/test')
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.message
+    })
+}
+
+/* Function to GET Web API Data*/
+const getWeatherData = async (baseURL, city, key)=>{
+  const res = await fetch(`${baseURL}${city}&appid=${key}&units=metric`);
+    
+  try {
+    const data = await res.json();
+    return data;
+  }  catch(error) {
+    console.log("error", error);
+  }
+}
+
+export { handleSubmit }
